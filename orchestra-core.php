1bd848bdcc80789fe6da91669c9e37f1bd395d3d
ldcnij0 <julian.nickerl@liebherr.com>
2026-08-25 14:20:21 +0200

    Add autoload support and automatic update checking

---
+++ orchestra-core.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Plugin Name: Orchestra Core
+ * Plugin URI:  https://github.com/TheRacoonOfDeath/orchestra-core
+ * Description: Core infrastructure plugin providing shared services for the Orchestra plugin suite.
+ * Version:     0.1.0
+ * Author:      Orchestra
+ * License:     GPL-2.0-or-later
+ * Text Domain: orchestra-core
+ * Requires PHP: 8.3
+ */
+
+declare(strict_types=1);
+
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+Orchestra\Core\Plugin::boot();
---
+++ src/Plugin.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orchestra\Core;
+
+class Plugin
+{
+    private static ?self $instance = null;
+
+    private function __construct()
+    {
+        $this->register();
+    }
+
+    public static function boot(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    private function register(): void
+    {
+        $this->registerUpdateChecker();
+    }
+
+    private function registerUpdateChecker(): void
+    {
+        $updateChecker = \YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateChecker(
+            'https://github.com/TheRacoonOfDeath/orchestra-core/',
+            dirname(__DIR__) . '/orchestra-core.php',
+            'orchestra-core'
+        );
+
+        $updateChecker->getVcsApi()->enableReleaseAssets();
+    }
+}
